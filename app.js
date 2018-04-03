76c9b70eea1f75b2943d5408ec333d09e93767ac
Jhovahn <jhovahn@Jhovahns-MacBook-Air.local>
2018-04-03 01:29:21 -0700

    finishing touches

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = require('path');
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
-app.use(express.static(path.join(__dirname, '/app.css')));
+
+app.use(express.static(__dirname + 'public'));
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.listen(app.get('port'), () =>
